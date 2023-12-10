e3cb1459c24ae7e73286ac2575c0ecbdbf2c1ac0
Ramazan Guner <ramazan.guner@haud.com>
2023-12-10 20:01:53 +0300

    getProductByTitleAndPrice

--- src/main/java/com/guner/repository/ProductRepository.java
+++ src/main/java/com/guner/repository/ProductRepository.java
@@ -3,9 +3,14 @@ package com.guner.repository;
 import com.guner.entity.Product;
 import com.guner.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    //Optional<Product> findByTitleAndPrice(String title, int price);
+
+    // yukarıdaki satır ile aynı işi yapar
+    @Query("select p from Product p where p.title = ?1 and p.price = ?2")
     Optional<Product> findByTitleAndPrice(String title, int price);
 }
